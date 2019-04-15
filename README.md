276bc68348222d1ab3594842b74fd21dbe8925e3
Eunchurn Park <eunchurn.park@gmail.com>
2019-04-15 21:17:26 +0900

    dev:first step

---
+++ README.md
@@ -0,0 +1,3 @@
+## Webpack for React Study
+
+- [Esau Silva](https://medium.freecodecamp.org/learn-webpack-for-react-a36d4cac5060) 튜토리얼 step 1 완료
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+    mode: "development",
+    output: {
+        filename: "bundle.[hash].js"
+    },
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            camelCase: true,
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "public/index.html",
+            favicon: "public/favicon.ico"
+        })
+    ],
+    devServer: {
+        host: "localhost",
+        port: port,
+        historyApiFallback: true,
+        open: true
+    }
+};
