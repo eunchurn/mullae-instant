7cdd4e240c2a83cadeae1a84428e3259d7f8b3f4
Eunchurn Park <eunchurn.park@gmail.com>
2019-08-16 04:07:06 +0900

    change folder

--- src/WinXP/src/WinXP/apps/Winamp/config.js
+++ src/WinXP/src/WinXP/apps/Winamp/config.js
@@ -5,7 +5,7 @@ const album = 'netBloc Vol. 24: tiuqottigeloot';
 export const initialTracks = [
   {
     url:
-      'https://github.com/we-underscore/mullaeinstant/raw/master/assets/Kevin_MacLeod_-_P_I_Tchaikovsky_Dance_of_the_Sugar_Plum_Fairy.mp3',
+      'https://raw.githubusercontent.com/we-underscore/mullaeinstant/master/assets/Kevin_MacLeod_-_P_I_Tchaikovsky_Dance_of_the_Sugar_Plum_Fairy.mp3',
     duration: 168.35,
     metaData: {
       title: 'P. I. Tchaikovsky: Dance of the Sugar Plum Fairy',
